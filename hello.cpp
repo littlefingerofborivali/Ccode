d9b9fb426e1c9963ea92f079cb3c11e2058f0eaf
littlefingerofborivali <suryapoojary@yandex.com>
2017-01-15 09:14:58 +0530

    hello world program added

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+cout << "hello world!!";
+
+
+}
+
+/* #include is for the preprocessor.<iostream> is the library file we will need for most of our C++ programs. int main() is the MAIN function in the program.The program starts executing in the main function. cout is used to print output to the screen, << is a part of the syntax.. and finally ; is used to end every statement in C++. */ 
