d6d4e372bde844584ba6be09f7349c5c13aa78c1
Alftron <mja.haycock@gmail.com>
2017-01-20 19:29:11 +0000

    Update hello.cpp
    I find it easier if the code is explained line by line rather than a block at the end!

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,13 @@
+// #include is for the preprocessor and <iostream> is the library file we will need for most of our C++ programs.
 #include <iostream>
 
 using namespace std;
 
+// int main() is the MAIN function in the program.The program starts executing in the main function. 
 int main(){
 
+// cout is used to print output to the screen, << is a part of the syntax.. and finally ; is used to end every statement in C++. 
 cout << "hello world!!";
-
-
 }
 
-/* #include is for the preprocessor.<iostream> is the library file we will need for most of our C++ programs. int main() is the MAIN function in the program.The program starts executing in the main function. cout is used to print output to the screen, << is a part of the syntax.. and finally ; is used to end every statement in C++. */ 
+
